57a69773a07679f3467978069e3849a6f62c1045
Thom2503 <thomveldhuis03@gmail.com>
2023-04-26 09:38:11 +0200

    Documentation toegevoegd aan PresentationLogic over hoe je nieuwe menu items maakt en wat je moet doen om een nieuwe presentation aan te maken

--- Logic/PresentationLogic.cs
+++ Logic/PresentationLogic.cs
@@ -1,3 +1,22 @@
+/// <summary>
+/// Dit is de <c>PresentationLogic</c> class, deze class zorgt er voor dat menu's goed worden opgebouwd.
+/// <example>
+/// Hoe je een menu item toevoegd, dit doe je in <c>Menu</c> hier is een lijst met <c>PresentationModels</c>
+/// daar voeg je dan een nieuw item aan toe zoals:
+/// <code>
+/// new PresentationModel(naam, beschrijving, welke clearance, presentation naam, bool voor submenu of niet),
+/// </code>
+/// Zorg er voor dat als je in een nieuwe presentation komt dat je dit doet:
+/// <code>
+/// PresentationLogic.CurrentPresentation = "movie";
+/// </code>
+/// dan als je <code> PresentationLogic.WriteMenu(Menu.presentationModels, true|false); </code> aanroept
+/// worden alleen de menu items van movie getoont.
+/// Als je een nieuwe presentation aanmaakt zorg dan dat deze de interface IPresentation overneemt, die
+/// class moet dan Start() implementeren, in deze start schrijf je het menu, maar maar je ook een "state machine",
+/// dit kan heel simpel door gewoon een opties aan een switch/if statement te doen. (Tip: kijk naar de andere presentation Start() methods).
+/// </example>
+/// </summary>
 public class PresentationLogic
 {
 	private static List<PresentationModel> _presentations = new List<PresentationModel>();
