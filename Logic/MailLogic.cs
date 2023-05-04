717ec7522dd9538b441ea654d0a0cbf0a411c3de
Voxxai <gilkranendonk@gmail.com>
2023-05-04 15:37:17 +0200

    Het versturen van een email werkt we kunnen later de format van het mailtje bepalen

--- Logic/MailLogic.cs
+++ Logic/MailLogic.cs
@@ -1,10 +1,37 @@
 using System;
 
-class MailCode
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+class MailLogic
 {
     static int GenerateCode()
     {
       Random RandomInt = new Random(); 
       return RandomInt.Next();
+    }
+
+    public static void SendMail()
+    {
+        var email = new MimeMessage();
+
+        email.From.Add(new MailboxAddress("Project Groep 1 INF1F", "projectgroep1fhr@gmail.com"));
+        email.To.Add(new MailboxAddress("Gilian", "gilkranendonk@gmail.com"));
+        email.Subject = "Test";
+        email.Body = new TextPart(MimeKit.Text.TextFormat.Html) 
+        { 
+          Text = "<b>Example HTML Message Body</b>" 
+        };
+
+        using (var smtp = new SmtpClient())
+        {
+          smtp.Connect("smtp.gmail.com", 587, false);
+
+          smtp.Authenticate("projectgroep1fhr@gmail.com", "gadaklozkmjfzcih");
+          smtp.Send(email);
+          Console.WriteLine("The mail has been sent successfully !!");
+          smtp.Disconnect(true);
+    }
     }
 }
