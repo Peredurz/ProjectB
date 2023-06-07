5256541de3e5c1a2b2c59116cfd3ca13c5fced46
Gilian Kranendonk <gilkranendonk@gmail.com>
2023-06-07 10:34:32 +0200

    #115 naam word nu 1x gevraagd na dat je een fout mailadres heb ingevoerd

--- Presentation/Mail.cs
+++ Presentation/Mail.cs
@@ -2,16 +2,23 @@ class Mail
 {
     public static Tuple<string, string> AskInfo()
     {
-        Console.WriteLine("Vul hier uw mail adres in");
-        Console.Write(">");
-        string mailAddress = Console.ReadLine();
-        bool validMailAddress = MailLogic.ValidateMailAddress(mailAddress);
-        if (validMailAddress == false)
+        bool loop = true;
+        string mailAddress = "";
+        while (loop)
         {
-            Console.WriteLine("Uw mail adres is niet geldig");
-            AskInfo();
+            Console.WriteLine("Vul hier uw mail adres in");
+            Console.Write(">");
+            mailAddress = Console.ReadLine();
+            bool validMailAddress = MailLogic.ValidateMailAddress(mailAddress);
+            if (validMailAddress == false)
+            {
+                Console.WriteLine("Uw mail adres is niet geldig");
+            }
+            else
+            {
+                loop = false;
+            }
         }
-
         Console.WriteLine("Vul hier uw naam in");
         Console.Write(">");
         string name = Console.ReadLine();
