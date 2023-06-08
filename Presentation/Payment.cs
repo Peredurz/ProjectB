3a8305c4c9913092335997b26d8c2e67fd6f9c40
Thom2503 <thomveldhuis03@gmail.com>
2023-06-08 19:58:30 +0200

    Als je enter drukte bij invullen van iban crashte die dus check ingebouwd voor issue #129

--- Presentation/Payment.cs
+++ Presentation/Payment.cs
@@ -12,7 +12,11 @@ public class Payment : IPresentation
                 Console.WriteLine("Vul hier jou Iban in.");
                 Console.Write(">");
                 string userIBAN = Console.ReadLine();
-                bool validIBAN = PaymentLogic.ValidateIBAN(userIBAN);
+                bool validIBAN;
+                if (userIBAN.Length <= 0)
+                    validIBAN = false;
+                else
+                    validIBAN = PaymentLogic.ValidateIBAN(userIBAN);
                 if (validIBAN == true)
                 {
                     Console.WriteLine("Betaling voltooid");
@@ -30,6 +34,8 @@ public class Payment : IPresentation
                         Console.WriteLine("Invalid invoer");
                         Console.Write(">");
                         string userIBAN1 = Console.ReadLine();
+                        if (userIBAN1.Length <= 0)
+                            continue;
                         bool checkIBAN = PaymentLogic.ValidateIBAN(userIBAN1);
                         if (checkIBAN == true)
                         {
