2fe0de3b5aeaae7af11ab8b8c623245663947465
Thom2503 <thomveldhuis03@gmail.com>
2023-06-09 15:31:00 +0200

    functie docs aan de chair reservation logic toegevoegd

--- Logic/ChairReservationLogic.cs
+++ Logic/ChairReservationLogic.cs
@@ -11,11 +11,17 @@ class ChairReservationLogic
         _chairReservation = ChairReservationAccess.LoadAll();
     }
 
+    /// <summary>
+    /// Om alle reserveringen naar de JSON te schrijven.
+    /// </summary>
     public void WriteAll()
     {
         ChairReservationAccess.WriteAll(_chairReservation);
     }
 
+    /// <summary>
+    /// Om een reservering voor een stoel te maken, deze returned True als deze gereserveerd is.
+    /// </summary>
     public bool ReserveChair(int auditoriumID, int movieID, ChairModel chair)
     {
         AccountsLogic.TotaalPrijs += chair.Price;
@@ -38,6 +44,9 @@ class ChairReservationLogic
         }
     }
 
+    /// <summary>
+    /// Om te checken of een stoel bezet is of niet.
+    /// </summary>
     public bool CheckChairAvailability(int id, DateTime time, int auditoriumId)
     {
         bool available = true;
@@ -53,6 +62,9 @@ class ChairReservationLogic
         return available;
     }
 
+    /// <summary>
+    /// Om een reservering te krijgen, dit kan via een reserveringscode, reserverings ID of stoel ID.
+    /// </summary>
     public Tuple<ChairReservationModel, int> GetChairReservation(int codeOrID)
     {
         foreach (ChairReservationModel _reservation in _chairReservation)
@@ -62,6 +74,10 @@ class ChairReservationLogic
         }
         return null;
     }
+
+    /// <summary>
+    /// Om meerdere stoelen te krijgen en dat als een lijst terug te geven.
+    /// </summary>
     public List<Tuple<ChairReservationModel, int>> GetChairReservations(int codeOrID)
     {
         List<Tuple<ChairReservationModel, int>> chairReservations = new List<Tuple<ChairReservationModel, int>>();
@@ -74,16 +90,26 @@ class ChairReservationLogic
         return chairReservations;
     }
 
+    /// <summary>
+    /// Om de index te krijgen van een reservering in de _chairReservation lijst.
+    /// </summary>
     public int GetChairReservationIndex(int reservationID)
     {
         return _chairReservation.FindIndex(x => x.ID == reservationID);
     }
 
+    /// <summary>
+    /// Om een stoel te updaten op een bepaalde index.
+    /// </summary>
     public void UpdateChairReservationAtIndex(Tuple<ChairReservationModel, int> reservationInfo)
     {
         _chairReservation[reservationInfo.Item2] = reservationInfo.Item1;
     }
 
+    /// <summary>
+    /// Om alle stoelen die gereserveerd zijn te updaten met nieuwe en gefinaliseerde data, deze verwijderd ook alle,
+    /// tijdelijke stoelen en update statische variables naar hun 0 waardes.
+    /// </summary>
     public static void UpdateChairReservation()
     {
         ChairReservationLogic chairReservationLogic = new ChairReservationLogic();
@@ -104,6 +130,9 @@ class ChairReservationLogic
         AccountsLogic.TotaalPrijs = 0;
     }
 
+    /// <summary>
+    /// Om alle niet afgemaakte reserveringen te verwijderen.
+    /// </summary>
     public void RemoveNotCompletedReservations()
     {
         List<ChairReservationModel> chairReservations = new List<ChairReservationModel>();
@@ -114,6 +143,9 @@ class ChairReservationLogic
         _chairReservation = chairReservations;
     }
 
+    /// <summary>
+    /// Om de reserveringen van de gebruiker te krijgen.
+    /// </summary>
     public List<ChairReservationModel> GetUserReservations(string emailAddress)
     {
         List<ChairReservationModel> chairReservations = new List<ChairReservationModel>();
